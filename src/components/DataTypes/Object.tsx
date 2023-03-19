50e7f01e51cc822df11d14424023261486234815
himself65 <himself65@outlook.com>
2023-03-18 22:25:32 -0500

    fix: eslint warnings

--- src/components/DataTypes/Object.tsx
+++ src/components/DataTypes/Object.tsx
@@ -81,7 +81,7 @@ export const PostObjectType: FC<DataItemProps<object>> = (props) => {
   const isArray = useMemo(() => Array.isArray(props.value), [props.value])
   const displayObjectSize = useJsonViewerStore(store => store.displayObjectSize)
   const isEmptyValue = useMemo(() => getValueSize(props.value) === 0, [props.value])
-  const sizeOfValue = useMemo(() => inspectMetadata(props.value), [props.inspect, props.value])
+  const sizeOfValue = useMemo(() => inspectMetadata(props.value), [props.value])
 
   return (
     <Box component='span' className='data-object-end'>
