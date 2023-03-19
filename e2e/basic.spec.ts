91476cb8fe63f00e512b880c122c309a333ca14e
himself65 <himself65@outlook.com>
2023-03-19 02:56:13 -0500

    test: add full example

--- e2e/basic.spec.ts
+++ e2e/basic.spec.ts
@@ -3,9 +3,9 @@ import { expect } from '@playwright/test'
 import { test } from './lib/playwright'
 
 test('click to hide', async ({ page }) => {
-  await page.goto('http://localhost:4000/full')
+  await page.goto('http://localhost:4000')
   const root = await page.getByTestId('data-key-pair')
-  const longArray = page.getByTestId('data-key-pairnumber')
+  const longArray = page.getByTestId('data-key-pairlongArray')
   expect(await longArray.nth(0).isVisible()).toBe(true)
   await root.click({
     position: {
