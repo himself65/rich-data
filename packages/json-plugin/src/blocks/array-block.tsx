56445f60c04b5f6153a52d8aea045f9c6bb7d4f4
himself65 <himself65@outlook.com>
2023-03-28 17:09:07 -0500

    feat(json-plugin): array block

--- packages/json-plugin/src/blocks/array-block.tsx
+++ packages/json-plugin/src/blocks/array-block.tsx
@@ -2,23 +2,72 @@ import type { DataValueProps } from '@rich-data/viewer'
 import { defineBlock } from '@rich-data/viewer'
 import { Metadata } from '@rich-data/viewer/components/metadata'
 import { usePath } from '@rich-data/viewer/hooks/use-path'
-import type { ReactElement } from 'react'
+import { IconChevronDown } from '@tabler/icons-react'
+import type { Dispatch, ReactElement, SetStateAction } from 'react'
+import React, { useState } from 'react'
+
+type ArrayBlockHeaderProps = {
+  expand: boolean
+  setExpand: Dispatch<SetStateAction<boolean>>
+}
+
+const ArrayBlockHeader: React.FC<ArrayBlockHeaderProps> = ({
+  expand,
+  setExpand
+}) => {
+  return (
+    <div className="array-block-header" onClick={
+      () => setExpand(expand => !expand)
+    }>
+      <IconChevronDown
+        className="rich-data--viewer-icon interactive"
+        transform={
+          expand ? 'rotate(0)' : 'rotate(-90)'
+        }/>
+      {!expand && (
+        <span>[...]</span>
+      )}
+    </div>
+  )
+}
+
+export type ArrayBlockBodyProps = {
+  expand: boolean
+}
+
+const ArrayBlockBody: React.FC<React.PropsWithChildren<ArrayBlockBodyProps>> = ({
+  expand,
+  children
+}) => {
+  if (!expand) {
+    return null
+  }
+  return (
+    <ul className="array-block-body">
+      {children}
+    </ul>
+  )
+}
 
 export function ArrayBlock (props: DataValueProps<unknown[]>): ReactElement {
   const value = props.value
   const Viewer = props.context.getViewer()
+  const [expand, setExpand] = useState(true)
   const currentPath = usePath(value).join('.')
   return (
     <Metadata flavour="official:array">
-      <ul className="array-block" data-object-path={currentPath}>
-        {value.map((item, index) => {
-          return (
-            <li key={index}>
-              <span>{index}</span> : <Viewer value={item}/>
-            </li>
-          )
-        })}
-      </ul>
+      <div className="array-block" data-object-path={currentPath}>
+        <ArrayBlockHeader expand={expand} setExpand={setExpand}/>
+        <ArrayBlockBody expand={expand}>
+          {value.map((item, index) => {
+            return (
+              <li key={index}>
+                <span>{index}</span> : <Viewer value={item}/>
+              </li>
+            )
+          })}
+        </ArrayBlockBody>
+      </div>
     </Metadata>
   )
 }
