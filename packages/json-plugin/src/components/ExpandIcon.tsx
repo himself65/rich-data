10f12e5c3fcf1ddce7e044e945e9af6358694595
himself65 <himself65@outlook.com>
2023-03-28 20:08:30 -0500

    fix: simplify bundle

--- packages/json-plugin/src/components/ExpandIcon.tsx
+++ packages/json-plugin/src/components/ExpandIcon.tsx
@@ -1,4 +1,6 @@
-import { IconChevronDown } from '@tabler/icons-react'
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-restricted-imports
+import IconChevronDown from '@tabler/icons-react/dist/esm/icons/IconChevronDown.js'
 import type { ReactElement } from 'react'
 
 export type ExpandIconProps = {
