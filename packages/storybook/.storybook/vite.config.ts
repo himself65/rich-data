6a8bc46b113c2cb28e522bf3ab9150ea8f67cf76
himself65 <himself65@outlook.com>
2023-04-01 01:47:36 -0500

    fix: scripts

--- packages/storybook/.storybook/vite.config.ts
+++ packages/storybook/.storybook/vite.config.ts
@@ -1,31 +1,33 @@
-import type { StorybookConfig } from '@storybook/react-vite';
-import { mergeConfig } from 'vite';
-import { resolve } from 'node:path';
-import { fileURLToPath } from 'url';
-import istanbul from 'vite-plugin-istanbul';
+import type { StorybookConfig } from '@storybook/react-vite'
+import { mergeConfig } from 'vite'
+import { resolve } from 'node:path'
+import { fileURLToPath } from 'url'
+import istanbul from 'vite-plugin-istanbul'
+import defineConfig from '@rich-data/scripts/vitest.config'
 
 const config: Pick<StorybookConfig, 'viteFinal'> = {
-  async viteFinal(config, { configType }) {
-    return mergeConfig(config, {
-      plugins: [
-        istanbul({
-          include: ['src/*'],
-          exclude: ['node_modules', 'test', 'stories'],
-          extension: ['.ts', '.tsx'],
-          forceBuildInstrument: process.env.COVERAGE === 'true',
-        }),
-      ],
-      resolve: {
-        alias: {
-          'react/jsx-runtime': 'react/jsx-runtime.js',
-          'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
-          '@rich-data/viewer': resolve(
-            fileURLToPath(new URL('../../viewer', import.meta.url))
-          ),
-        },
-      },
-    });
-  },
-};
+  async viteFinal (config, { configType }) {
+    return mergeConfig(config, mergeConfig(defineConfig(), {
+        plugins: [
+          istanbul({
+            include: ['src/*'],
+            exclude: ['node_modules', 'test', 'stories'],
+            extension: ['.ts', '.tsx'],
+            forceBuildInstrument: process.env.COVERAGE === 'true'
+          })
+        ],
+        resolve: {
+          alias: {
+            'react/jsx-runtime': 'react/jsx-runtime.js',
+            'react/jsx-dev-runtime': 'react/jsx-dev-runtime.js',
+            '@rich-data/viewer': resolve(
+              fileURLToPath(new URL('../../viewer', import.meta.url))
+            )
+          }
+        }
+      })
+    )
+  }
+}
 
-export default config;
+export default config
