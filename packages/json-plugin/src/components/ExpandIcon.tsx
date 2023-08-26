98ccf3182d1a876568c24f4f073ab8ca9221b5ad
fourdim <59462000+fourdim@users.noreply.github.com>
2023-08-26 13:57:40 +0800

    feat: remove @tabler/icons-react
    This will remove 157M of dependency from blocksuite

--- packages/json-plugin/src/components/ExpandIcon.tsx
+++ packages/json-plugin/src/components/ExpandIcon.tsx
@@ -1,9 +1,31 @@
 import type { ReactElement } from 'react'
-import { lazy } from 'react'
 
-// @ts-ignore
-// eslint-disable-next-line @typescript-eslint/no-restricted-imports
-const IconChevronDown = lazy(() => import('@tabler/icons-react/dist/esm/icons/IconChevronDown.js'))
+// https://github.com/konradkalemba/tabler-icons-react MIT License
+const IconChevronDown = 
+({
+  size = 24,
+  color = 'currentColor',
+  ...restProps
+}) => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      className='icon icon-tabler icon-tabler-chevron-down'
+      width={size}
+      height={size}
+      viewBox='0 0 24 24'
+      stroke={color}
+      strokeWidth='2'
+      fill='none'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+      {...restProps}
+    >
+      <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+      <polyline points='6 9 12 15 18 9' />
+    </svg>
+  );
+}
 
 export type ExpandIconProps = {
   expand: boolean
